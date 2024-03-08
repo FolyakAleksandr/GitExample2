83f1a4f41b55ecc839d2f1b67135e77819843515
Aleksandr <a.folyak2000@gmail.com>
2024-03-08 18:37:20 +0600

    first commit

---
+++ GitTest1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitTest1
+//
+//  Created by Александр Фоляк on 08.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
